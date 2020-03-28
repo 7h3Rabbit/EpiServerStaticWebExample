44937a111ede0c312100775756448cbdb2862202
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2020-03-28 12:54:19 +0100

    now supporting static resources ending with allowed file extension
    Allowed extensions are:
    .css
    .js
    .png
    .jpg
    .jpeg
    .jpe
    .gif
    .bmp
    .ico

--- EpiserverStaticWeb/Business/StaticWebEventInit.cs
+++ EpiserverStaticWeb/Business/StaticWebEventInit.cs
@@ -41,90 +41,6 @@ namespace EpiserverStaticWeb.Business
                 var contentLink = e.ContentLink;
 
                 GeneratePage(contentLink);
-
-                //var page = e.Content; // as PageData;
-                ////var urlResolver = ServiceLocator.Current.GetInstance<UrlResolver>();
-                ////var page2 = urlResolver.Route(new EPiServer.UrlBuilder(httpContext.Request.Url));
-
-                //// Resolve the right Template based on EpiServers own templating engine
-                //var templateResolver = ServiceLocator.Current.GetInstance<TemplateResolver>();
-                //var model = templateResolver.ResolvePageTemplate(httpContextBase, page);
-
-                //// Resolve the controller
-                //var pageController = ServiceLocator.Current.GetInstance(model.TemplateType) as ControllerBase;
-                //string controllerName = pageController.GetType().Name.Replace("Controller", string.Empty);
-
-                //// Create routing data for controller
-                //RouteData routeData = new RouteData();
-                //routeData.Values.Add("currentPage", page);
-                //routeData.Values.Add(RoutingConstants.CurrentContentKey, model);
-                //routeData.Values.Add(RoutingConstants.ControllerTypeKey, model.TemplateType);
-                //// TODO: Modify this to use available language(s)
-                //routeData.Values.Add(RoutingConstants.LanguageKey, ContentLanguage.PreferredCulture.Name);
-                //routeData.Values.Add(RoutingConstants.ControllerKey, controllerName);
-                //routeData.Values.Add(RoutingConstants.NodeKey, page.ContentLink);
-                //routeData.Values.Add("contentLink", page.ContentLink);
-                //routeData.Values.Add(RoutingConstants.PartialKey, page.ContentLink);
-                //routeData.Values.Add(RoutingConstants.ActionKey, "index");
-
-
-                //var controllerFactory = ServiceLocator.Current.GetInstance<IControllerFactory>();
-                //var controller = (ControllerBase)controllerFactory.CreateController(httpContextBase.Request.RequestContext, controllerName);
-
-                ////var layoutController = controller as IModifyLayout;
-                ////if (layoutController != null)
-                ////{
-                ////    PageViewContextFactory _contextFactory = new PageViewContextFactory()
-                ////    model.Layout = _contextFactory.CreateLayoutModel(page.ContentLink, viewContext.RequestContext);
-
-                ////    layoutController.ModifyLayout(layoutModel);
-                ////}
-
-
-
-                //var controllerContext = new ControllerContext(httpContextBase, routeData, controller);
-                //var controllerDescriptor = new ReflectedControllerDescriptor(controller.GetType());
-
-                //var actionDescriptor = controllerDescriptor.FindAction(controllerContext, "index");
-                ////var actionViewResult = actionDescriptor.Execute(controllerContext, new Dictionary<string, object>() { { "currentPage", page } }) as ViewResult;
-                //var actionViewResult = actionDescriptor.Execute(controllerContext, new Dictionary<string, object>() {
-                //    { "currentPage", page },
-                //    { "contentLink", page.ContentLink },
-                //    { RoutingConstants.CurrentContentKey, model },
-                //    { RoutingConstants.ControllerTypeKey, model.TemplateType },
-                //    { RoutingConstants.LanguageKey, ContentLanguage.PreferredCulture.Name },
-                //    { RoutingConstants.ControllerKey, controllerName },
-                //    { RoutingConstants.NodeKey, page.ContentLink },
-                //    { RoutingConstants.PartialKey, page.ContentLink },
-                //    { RoutingConstants.ActionKey, "index" },
-                //    { RoutingConstants.RoutedDataKey, page.ContentLink }
-                //}) as ViewResult;
-                //var viewResult = actionViewResult.ViewEngineCollection.FindView(controllerContext, actionViewResult.ViewName, actionViewResult.MasterName);
-
-
-                //using (var stringWriter = new StringWriter())
-                //{
-                //    var viewContext = new ViewContext(controllerContext, viewResult.View, actionViewResult.ViewData, actionViewResult.TempData, stringWriter);
-                //    viewContext.RouteData = routeData;
-                //    viewContext.RequestContext.RouteData = routeData;
-
-                //    viewContext.RequestContext.RouteData.DataTokens.Add("currentPage", page);
-                //    viewContext.RequestContext.RouteData.DataTokens.Add("contentLink", page.ContentLink);
-
-                //    viewContext.RequestContext.RouteData.DataTokens.Add(RoutingConstants.CurrentContentKey, model);
-                //    viewContext.RequestContext.RouteData.DataTokens.Add(RoutingConstants.ControllerTypeKey, model.TemplateType);
-                //    viewContext.RequestContext.RouteData.DataTokens.Add(RoutingConstants.LanguageKey, ContentLanguage.PreferredCulture.Name);
-                //    viewContext.RequestContext.RouteData.DataTokens.Add(RoutingConstants.ControllerKey, controllerName);
-                //    viewContext.RequestContext.RouteData.DataTokens.Add(RoutingConstants.NodeKey, page.ContentLink);
-                //    viewContext.RequestContext.RouteData.DataTokens.Add(RoutingConstants.PartialKey, page.ContentLink);
-                //    viewContext.RequestContext.RouteData.DataTokens.Add(RoutingConstants.ActionKey, "index");
-                //    viewContext.RequestContext.RouteData.DataTokens.Add(RoutingConstants.RoutedDataKey, page.ContentLink);
-
-                //    viewResult.View.Render(viewContext, stringWriter);
-                //    var html = stringWriter.ToString();
-                //    // TODO: Where to write content?
-                //    System.IO.File.WriteAllText(@"C:\inetpub\wwwroot\index.html", html);
-                //}
             }
             else if (e.Content is BlockData)
             {
@@ -136,18 +52,7 @@ namespace EpiserverStaticWeb.Business
                 foreach (var page in pages)
                 {
                     GeneratePage(page.ContentLink);
-
                 }
-
-                //foreach (var reference in references)
-                //{
-                //    IContent page;
-                //    //var contentLink = new ContentReference(reference.ReferencedID.ID, false);
-                //    if (repository.TryGet<IContent>(reference.OwnerID, out page))
-                //    {
-                //        GeneratePage(page.ContentLink);
-                //    }
-                //}
             }
         }
 
@@ -157,6 +62,8 @@ namespace EpiserverStaticWeb.Business
             var orginalUrl = urlResolver.GetUrl(contentLink);
             if (orginalUrl == null)
                 return;
+
+            var rootPath = @"C:\inetpub\wwwroot";
 
             var relativePath = orginalUrl.Replace("/", @"\");
             if (!relativePath.StartsWith(@"\"))
@@ -172,16 +79,55 @@ namespace EpiserverStaticWeb.Business
             System.Net.WebClient webClient = new System.Net.WebClient();
             var html = webClient.DownloadString("http://localhost:49822/" + orginalUrl);
 
-            // TODO: make sure we have all resources for current page also
-            // <(script|link|img|source).*(href|src|srcset)="(?<resource>[^"]+)"
+            // TODO: make sure we have all resources for current page
+            // <(script|link|img).*(href|src)="(?<resource>[^"]+)
+            var matches = System.Text.RegularExpressions.Regex.Matches(html, "<(script|link|img).*(href|src)=\"(?<resource>[^\"]+)");
+            foreach (System.Text.RegularExpressions.Match match in matches)
+            {
+                var group = match.Groups["resource"];
+                if (group.Success)
+                {
+                    var value = group.Value;
+                    if (value.StartsWith("/"))
+                    {
+                        // TODO: check if local file or db file
+                        // TODO: download resource
+                        // NOTE: Temporary fix for approved file formats
+                        if (value.EndsWith(".css")
+                            || value.EndsWith(".js")
+                            || value.EndsWith(".png")
+                            || value.EndsWith(".jpg")
+                            || value.EndsWith(".jpeg")
+                            || value.EndsWith(".jpe")
+                            || value.EndsWith(".gif")
+                            || value.EndsWith(".bmp")
+                            || value.EndsWith(".ico"))
+                        {
+                            System.Net.WebClient resourceClient = new System.Net.WebClient();
+                            //byte[] data = resourceClient.DownloadData("http://localhost:49822/" + value);
+                            // TODO: check file extension and change filename
+                            //resourceClient.ResponseHeaders.
+
+                            var directory = System.IO.Path.GetDirectoryName(rootPath + value.Replace("/", "\\"));
+                            if (!Directory.Exists(directory))
+                            {
+                                Directory.CreateDirectory(directory);
+                            }
+
+                            resourceClient.DownloadFile("http://localhost:49822/" + value, rootPath + value.Replace("/", "\\"));
+                        }
+                    }
+                }
+            }
+
+            // TODO: make sure we have all source resources for current page
+            // <(source).*(srcset)="(?<resource>[^"]+)"
 
             // TODO: make sure we have all meta resources for current page
             // Below matches ALL meta content that is a URL
             // <(meta).*(content)="(?<resource>(http:\/\/|https:\/\/|\/)[^"]+)"
             // Below matches ONLY known properties
             // <(meta).*(property|name)="(twitter:image|og:image)".*(content)="(?<resource>[http:\/\/|https:\/\/|\/][^"]+)"
-
-            var rootPath = @"C:\inetpub\wwwroot";
 
             if (!System.IO.Directory.Exists(rootPath + relativePath))
             {
