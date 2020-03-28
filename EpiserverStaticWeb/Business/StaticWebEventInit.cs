1773881a2f1f53c43a16aafd6e2b3e132697dc28
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2020-03-28 18:46:00 +0100

    we can now handle dependencies in css files + refactoring

--- EpiserverStaticWeb/Business/StaticWebEventInit.cs
+++ EpiserverStaticWeb/Business/StaticWebEventInit.cs
@@ -3,17 +3,15 @@ using EPiServer.Core;
 using EPiServer.Filters;
 using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
-using EPiServer.Globalization;
 using EPiServer.ServiceLocation;
-using EPiServer.Web;
 using EPiServer.Web.Routing;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
 
 namespace EpiserverStaticWeb.Business
 {
@@ -27,7 +25,7 @@ namespace EpiserverStaticWeb.Business
             events.PublishedContent += OnPublishedContent;
         }
 
-        private void OnPublishedContent(object sender, EPiServer.ContentEventArgs e)
+        private void OnPublishedContent(object sender, ContentEventArgs e)
         {
             var httpContext = HttpContext.Current;
 
@@ -64,6 +62,7 @@ namespace EpiserverStaticWeb.Business
                 return;
 
             var rootPath = @"C:\inetpub\example-site\EpiServerStaticWebExampleResultWebSite";
+            var rootUrl = "http://localhost:49822/";
 
             var relativePath = orginalUrl.Replace("/", @"\");
             if (!relativePath.StartsWith(@"\"))
@@ -74,49 +73,33 @@ namespace EpiserverStaticWeb.Business
             {
                 relativePath = relativePath + @"\";
             }
-            relativePath = relativePath;
 
-            System.Net.WebClient webClient = new System.Net.WebClient();
-            var html = webClient.DownloadString("http://localhost:49822/" + orginalUrl);
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            var html = webClient.DownloadString(rootUrl + orginalUrl);
 
+            EnsurePageResources(rootUrl, rootPath, html);
+
+            if (!Directory.Exists(rootPath + relativePath))
+            {
+                Directory.CreateDirectory(rootPath + relativePath);
+            }
+
+            File.WriteAllText(rootPath + relativePath + "index.html", html);
+        }
+
+        private static void EnsurePageResources(string rootUrl, string rootPath, string html)
+        {
             // TODO: make sure we have all resources for current page
             // <(script|link|img).*(href|src)="(?<resource>[^"]+)
-            var matches = System.Text.RegularExpressions.Regex.Matches(html, "<(script|link|img).*(href|src)=\"(?<resource>[^\"]+)");
-            foreach (System.Text.RegularExpressions.Match match in matches)
+            var matches = Regex.Matches(html, "<(script|link|img).*(href|src)=\"(?<resource>[^\"]+)");
+            foreach (Match match in matches)
             {
                 var group = match.Groups["resource"];
                 if (group.Success)
                 {
-                    var value = group.Value;
-                    if (value.StartsWith("/"))
-                    {
-                        // TODO: check if local file or db file
-                        // TODO: download resource
-                        // NOTE: Temporary fix for approved file formats
-                        if (value.EndsWith(".css")
-                            || value.EndsWith(".js")
-                            || value.EndsWith(".png")
-                            || value.EndsWith(".jpg")
-                            || value.EndsWith(".jpeg")
-                            || value.EndsWith(".jpe")
-                            || value.EndsWith(".gif")
-                            || value.EndsWith(".bmp")
-                            || value.EndsWith(".ico"))
-                        {
-                            System.Net.WebClient resourceClient = new System.Net.WebClient();
-                            //byte[] data = resourceClient.DownloadData("http://localhost:49822/" + value);
-                            // TODO: check file extension and change filename
-                            //resourceClient.ResponseHeaders.
-
-                            var directory = System.IO.Path.GetDirectoryName(rootPath + value.Replace("/", "\\"));
-                            if (!Directory.Exists(directory))
-                            {
-                                Directory.CreateDirectory(directory);
-                            }
-
-                            resourceClient.DownloadFile("http://localhost:49822/" + value, rootPath + value.Replace("/", "\\"));
-                        }
-                    }
+                    var resourceUrl = group.Value;
+                    EnsureResource(rootUrl, rootPath, resourceUrl);
                 }
             }
 
@@ -128,13 +111,127 @@ namespace EpiserverStaticWeb.Business
             // <(meta).*(content)="(?<resource>(http:\/\/|https:\/\/|\/)[^"]+)"
             // Below matches ONLY known properties
             // <(meta).*(property|name)="(twitter:image|og:image)".*(content)="(?<resource>[http:\/\/|https:\/\/|\/][^"]+)"
+        }
 
-            if (!System.IO.Directory.Exists(rootPath + relativePath))
+        private static void EnsureResource(string rootUrl, string rootPath, string resourceUrl)
+        {
+            if (resourceUrl.StartsWith("/"))
             {
-                System.IO.Directory.CreateDirectory(rootPath + relativePath);
-            }
+                //switch (Path.GetExtension(resourceUrl).ToLower())
+                //{
+                //    case ".css":
+                //        break;
+                //    case ".js":
+                //    case ".woff":
+                //    case ".woff2":
+                //    case ".png":
+                //    case ".jpg":
+                //    case ".jpeg":
+                //    case ".jpe":
+                //    case ".gif":
+                //    case ".ico":
+                //    case ".pdf":
+                //        break;
+                //    case ".bmp":
+                //        break;
+                //}
+                // TODO: check if local file or db file
+                // TODO: download resource
+                if (resourceUrl.EndsWith(".css"))
+                {
+                    EnsureCssResources(rootUrl, rootPath, resourceUrl);
+                }
+                else if (resourceUrl.EndsWith(".js")
+                || resourceUrl.EndsWith(".woff2")
+                || resourceUrl.EndsWith(".woff")
+                || resourceUrl.EndsWith(".png")
+                || resourceUrl.EndsWith(".webp")
+                || resourceUrl.EndsWith(".jpg")
+                || resourceUrl.EndsWith(".jpeg")
+                || resourceUrl.EndsWith(".jpe")
+                || resourceUrl.EndsWith(".gif")
+                || resourceUrl.EndsWith(".ico")
+                || resourceUrl.EndsWith(".pdf"))
+                {
+                    // For approved file extensions that we don't want to do any changes on
+                    var downloadUrl = rootUrl + resourceUrl;
+                    var filepath = rootPath + resourceUrl.Replace("/", "\\");
 
-            System.IO.File.WriteAllText(rootPath + relativePath + "index.html", html);
+                    DownloadFile(downloadUrl, filepath);
+                }
+                else if (resourceUrl.EndsWith(".bmp")
+                    || resourceUrl.EndsWith(".mp4")
+                    || resourceUrl.EndsWith(".flv")
+                    || resourceUrl.EndsWith(".webm"))
+                {
+                    // don't download files of this extensions
+                }
+                else if (resourceUrl.EndsWith(".html")
+                    || resourceUrl.EndsWith(".htm")
+                    || resourceUrl.EndsWith("/"))
+                {
+                    // don't download web pages
+                }
+                else
+                {
+                    // TODO: Download and look if we recognize content-type
+                }
+            }
+        }
+
+        private static void EnsureCssResources(string rootUrl, string rootPath, string url)
+        {
+            // TODO: Download and ensure files referenced are downloaded also
+            WebClient referencableClient = new WebClient();
+            referencableClient.Encoding = Encoding.UTF8;
+            byte[] data = referencableClient.DownloadData(rootUrl + url);
+            var content = Encoding.UTF8.GetString(data);
+
+            var matches = Regex.Matches(content, "url\\([\"|']{0,1}(?<resource>[^[\\)\"|']+)");
+            foreach (Match match in matches)
+            {
+                var group = match.Groups["resource"];
+                if (group.Success)
+                {
+                    var resourceUrl = group.Value;
+                    var directory = url.Substring(0, url.LastIndexOf('/'));
+                    var changedDir = false;
+                    while (resourceUrl.StartsWith("../"))
+                    {
+                        changedDir = true;
+                        resourceUrl = resourceUrl.Remove(0, 3);
+                        directory = directory.Substring(0, directory.LastIndexOf('/'));
+                    }
+
+                    if (changedDir)
+                    {
+                        resourceUrl = directory.Replace(@"\", "/") + "/" + resourceUrl;
+                    }
+
+                    DownloadFile(rootUrl + resourceUrl, rootPath + resourceUrl.Replace("/", @"\"));
+                }
+            }
+            // TODO: support download of referenced resources
+            // url\(["|']{0,1}(?<resource>[^[\)"|']+)
+
+            // TODO: check file extension and change filename
+            //resourceClient.ResponseHeaders.
+        }
+
+        private static void DownloadFile(string downloadUrl, string filepath)
+        {
+            using (WebClient resourceClient = new WebClient())
+            {
+                resourceClient.Encoding = Encoding.UTF8;
+
+                var directory = Path.GetDirectoryName(filepath);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                resourceClient.DownloadFile(downloadUrl, filepath);
+            }
         }
 
         public List<PageData> GetPageReferencesToContent(IContentRepository repository, ContentReference contentReference)
