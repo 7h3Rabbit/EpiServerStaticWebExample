0dc481682b1341b1266ae86de710c83b3e9f4e02
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2020-03-30 11:20:44 +0200

    Split the functionality out as its own project
    Solves #4.

---
+++ StaticWebEpiserverPlugin/Services/IStaticWebService.cs
@@ -0,0 +1,11 @@
+﻿using EPiServer.Core;
+using System.Globalization;
+
+namespace StaticWebEpiserverPlugin.Services
+{
+    public interface IStaticWebService
+    {
+        void GeneratePage(ContentReference contentLink, CultureInfo language);
+        void GeneratePagesDependingOnBlock(ContentReference contentLink);
+    }
+}
