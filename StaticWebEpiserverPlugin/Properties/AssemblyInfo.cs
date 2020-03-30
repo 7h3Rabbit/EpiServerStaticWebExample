ed71882aef8230c1f226c330ed4586828249dd84
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2020-03-30 13:15:46 +0200

    starting work on NuGet
    relates to issue #5

--- StaticWebEpiserverPlugin/Properties/AssemblyInfo.cs
+++ StaticWebEpiserverPlugin/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StaticWebEpiserverPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("StaticWeb Episerver Plugin")]
+[assembly: AssemblyDescription("Generate static webpages on publish event using EpiServer as CMS for editors")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("7h3Rabbit")]
 [assembly: AssemblyProduct("StaticWebEpiserverPlugin")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
